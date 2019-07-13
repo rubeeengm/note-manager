6790cbbe12359bd9d65a812e650e90a727a50d39
rubeeengm <rubeeengm@gmail.com>
2019-07-13 02:07:41 -0500

    smaller code and bug and bug fix in the event

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 var newListItem = document.createElement('li');
 
 var firstParragraph = document.createElement('p');
-//firstParragraph.appendChild(document.createTextNode('hola'));
 
 var secondParragraph = document.createElement('p');
 
 var edit = document.createElement('i');
-edit.setAttribute('class', 'fa fa-pencil-square-o');
+edit.className = 'fa fa-pencil-square-o';
 
 var del = document.createElement('i');
-del.setAttribute('class', 'fa fa-times');
+del.className = 'fa fa-times';
 
 var input = document.createElement('input');
-input.setAttribute('class', 'edit-note');
+input.className = 'edit-note';
 input.setAttribute('type', 'text');
 
 secondParragraph.appendChild(edit);
 secondParragraph.appendChild(del);
-secondParragraph.appendChild(input);
 
 newListItem.appendChild(firstParragraph);
 newListItem.appendChild(secondParragraph);
+newListItem.appendChild(input);
 
 var list = document.getElementById('list');
 
@@ -30,7 +29,10 @@ var inputText = document.getElementById('add-input');
 
 btn.addEventListener('click', function(e) {
     e.preventDefault();
-    firstParragraph.appendChild(document.createTextNode(inputText.value));
-    list.appendChild(newListItem);
-    inputText.value = '';
-})
+    
+    if(inputText.value !== '') {
+        firstParragraph.textContent = inputText.value;
+        list.appendChild(newListItem);
+        inputText.value = '';
+    }
+});
