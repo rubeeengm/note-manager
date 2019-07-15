aa9dce83019af15e6033c625f304bec5501c588f
rubeeengm <rubeeengm@gmail.com>
2019-07-15 00:45:36 -0500

    event remove

--- script.js
+++ script.js
@@ -49,5 +49,21 @@ list.addEventListener('click', function(e) {
         //show input
         input.style.display = 'block';
         input.value = note.textContent;
+
+        input.addEventListener('keypress', function(e) {
+            if(e.keyCode === 13) {
+                if(input.value !== '') {
+                    note.textContent = input.value;
+                    parentPar.style.display = 'block';
+                    input.style.display = 'none';
+                } else {
+                    var li = input.parentNode;
+                    li.parentNode.removeChild(li);
+                }
+            }
+        });
+    } else if (e.target.classList[1] === 'fa-times') {
+        var list = e.target.parentNode.parentNode;
+        list.parentNode.removeChild(list);
     }
 });
